8b6847c991bf0492d865fad4af3cf5692d86ab93
Chris Sev <chris@better.dev>
2021-02-10 09:14:48 -0800

    stuff!

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e7e63cbd647cebf48885399d83e540ac'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3b9d6909-bede-4721-a4a8-4bb79cd3720e'
+};
